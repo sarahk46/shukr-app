6eac393e98b191e0e2f52b03ab3683455e5ee345
sarahk46 <sarahk46@uw.edu>
2023-02-26 22:28:51 -0800

    Added pages and icons from MaterialCommunityIcons

---
+++ Home.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home page</Text>
+    </View>
+  );
+}
+
+export default Home;
---
+++ Settings.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function Settings() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings page</Text>
+    </View>
+  );
+}
+
+export default Settings;
