11fe80a5ff643b86c9ed50f98645d19673118b2e
sarahk46 <sarahk46@uw.edu>
2023-02-27 17:43:15 -0800

    Added button widget with some element of styling

--- Home.js
+++ Home.js
@@ -1,11 +1,48 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Card, Button, Text } from '@rneui/themed';
+import { NavigationContainer } from '@react-navigation/native';
 
-function Home() {
+function Home({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home page</Text>
+    <View style={[styles.container, { justifyContent: 'flex-start', alignItems: 'center' }]}>
+      <Card>
+        <Card.Title style={styles.title}>Hadith of the Day</Card.Title>
+        <Card.Divider />
+        <Text style={styles.fonts} h2>
+            Insert Arabic text here
+        </Text>
+        <Text style={styles.fonts} h3>
+            Insert English text here
+        </Text>
+
+        <Button
+            color='#cab1bd'
+            style={styles.buttonStyle}
+            title="Reflect Now"
+            onPress={() => alert('Pressed')}
+          />
+
+      </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+  },
+  fonts: {
+    marginBottom: 12,
+    fontSize: 10,
+    textAlign: 'center',
+  },
+
+  buttonStyle: {
+    borderRadius: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+  }
+});
 
 export default Home;
