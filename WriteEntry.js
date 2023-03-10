68e437fb4a0444e9de7b4d29e65e8041324c2151
sarahk46 <sarahk46@uw.edu>
2023-03-10 12:27:44 -0800

    Still working on

--- WriteEntry.js
+++ WriteEntry.js
@@ -9,19 +9,23 @@ function WriteEntry() {
     // TODO: Probably separate out the hadith card in its own component
     // Second display the actual questions -- need to figure out styling in separate area
     // Maybe want a box for each question + answer too
+    // Gotta also add scrollview and account for lack of button in styling
+    // for hadithBox
+    // Some issues in the text display
     <NativeBaseProvider>
       <Box style={styles.hadithBox}>
           <Text bold italic style={styles.title}>HADITH OF THE DAY</Text>
           <Text style={styles.hadithTextArabic}> 
   دَّثَنَا قَبِيصَةُ، حَدَّثَنَا سُفْيَانُ، عَنْ أَبِي حَازِمٍ، عَنْ سَهْلِ بْنِ سَعْدٍ ـ رضى الله عنه ـ عَنِ النَّبِيِّ صلى الله عليه وسلم قَالَ " الرَّوْحَةُ وَالْغَدْوَةُ فِي سَبِيلِ اللَّهِ أَفْضَلُ مِنَ الدُّنْيَا وَمَا فِيهَا</Text>
-          <Text style={styles.citationText}> (Bukhari, 2794) </Text>
-          <Text style={styles.hadithTextEnglish}>Narrated Sahl bin Sa`d: The Prophet (ﷺ) said, "A single endeavor in Allah's Cause in the afternoon and in the forenoon is better than the world and whatever is in it.”</Text>
+
+          <Text> (Bukhari, 2794) </Text>
+          <Text>Narrated Sahl bin Sa`d: The Prophet (ﷺ) said, "A single endeavor in Allah's Cause in the afternoon and in the forenoon is better than the world and whatever is in it.”</Text>
       </Box>
       <Divider />
-          <Heading size="md">column</Heading>
-          <Stack mb="2.5" mt="1.5" direction="column" space={3}>
+          <Heading size="md">Reflection</Heading>
+          <Stack direction="column" space={3}>
             <Text>Question 1 Text</Text>
-            <Input h = "100" variant="outline" placeholder="Question 1 Answer" />
+            <Input variant="outline" placeholder="Question 1 Answer" />
             <Divider />
             <Text>Question 2 Text</Text>
             <Input variant="outline" placeholder="Question 2 Answer" />
