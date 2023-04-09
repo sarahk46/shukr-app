dc2194445e4a944ae46ac20fb81e99225dba9ff6
rafiak <rafiak@uw.edu>
2023-04-08 17:43:52 -0700

    resolve package dependencies in package.json by updating react and react-dom, install ESLint and Prettier

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 ## Shukr
+
 <!-- GETTING STARTED -->
+
 ## Getting Started
 
-
 ### Resources:
+
 UI library: <a href="https://reactnativeelements.com/">React Native Elements</a>
 Icons: <a href="https://pictogrammers.com/library/mdi/">MaterialCommunityIcons</a>
 
 ### Development Guidelines:
+
 Whenever you want to make an edit to the repo, do the following:
-1. Make a new branch with the new branch starting w/ your initials and a slash : ```git checkout -b sk/[insert rest of branch name here]```
-2. After adding + committing changes, run following command: ```git push --set-upstream origin sk/[your branch name from step 1 here]```
+
+1. Make a new branch with the new branch starting w/ your initials and a slash : `git checkout -b sk/[insert rest of branch name here]`
+2. After adding + committing changes, run following command: `git push --set-upstream origin sk/[your branch name from step 1 here]`
 3. Go to GitHub website and go to "Pull requests"
 4. Create a new pull request for your branch + add any important info
 5. Send message to rest of chat to review PR + be able to merge it into main
@@ -20,7 +24,7 @@ Whenever you want to make an edit to the repo, do the following:
 You can get a local copy up and running with an EMULATOR or through the EXPO GO APP:
 
 💻 EMULATOR: if you don't have Android Studio already installed, follow the directions <a href="https://developer.android.com/studio/install#:~:text=Launch%20the%20Android%20Studio%20DMG,Studio%20settings%2C%20then%20click%20OK.">here</a>
-*System requirments: check the bottom of this <a href="https://developer.android.com/studio/install#:~:text=Launch%20the%20Android%20Studio%20DMG,Studio%20settings%2C%20then%20click%20OK.">page</a>.
+\*System requirments: check the bottom of this <a href="https://developer.android.com/studio/install#:~:text=Launch%20the%20Android%20Studio%20DMG,Studio%20settings%2C%20then%20click%20OK.">page</a>.
 
 📱 EXPO GO: Download on your iPhone or Android.
 
@@ -36,7 +40,7 @@ Once you have either the emulator or Expo Go installed, you can run it on your l
    ```sh
    npm install / yarn install
    ```
-3. Start the app 
+3. Start the app
    ```sh
    expo start / yarn start
    ```
--- Screens/ViewEntry.js
+++ Screens/ViewEntry.js
@@ -1,6 +1,16 @@
 import React, { useContext, useState } from 'react';
 import { TextInput } from 'react-native';
-import { Box, Button, ScrollView, Stack, Text, TextArea, Icon, ChevronLeftIcon, View } from 'native-base';
+import {
+  Box,
+  Button,
+  ScrollView,
+  Stack,
+  Text,
+  TextArea,
+  Icon,
+  ChevronLeftIcon,
+  View,
+} from 'native-base';
 import JournalContext from '../JournalContext';
 import HadithCard from '../Components/HadithCard';
 import styles from '../styles';
@@ -9,15 +19,13 @@ import { MaterialCommunityIcons } from 'react-native-vector-icons';
 
 function displayTopBar(date, isEditing) {
   if (isEditing) {
-    return (
-      <Text style={styles.dateTitle}>{date}</Text>
-    );
+    return <Text style={styles.dateTitle}>{date}</Text>;
   }
   return (
     <View flexDirection="row" alignItems="center" justifyContent="space-between" marginBottom="5">
       <Button
         startIcon={<ChevronLeftIcon />}
-        _icon={{ color: "black" }}
+        _icon={{ color: 'black' }}
         style={styles.iconButton}
         onPress={() => navigation.navigate('Your Journal')}
       />
@@ -34,9 +42,7 @@ function ViewEntry({ route, navigation }) {
 
   const handleSave = () => {
     setJournalEntries(
-      journalEntries.map((entry) =>
-        entry.id === editedEntry.id ? editedEntry : entry
-      )
+      journalEntries.map((entry) => (entry.id === editedEntry.id ? editedEntry : entry))
     );
     setOriginalEntry(editedEntry);
     setIsEditing(false);
@@ -49,65 +55,67 @@ function ViewEntry({ route, navigation }) {
 
   function EditEntry() {
     return (
-    <>
-        <Text fontSize="16" alignText="center">{route.params.date}</Text>
+      <>
+        <Text fontSize="16" alignText="center">
+          {route.params.date}
+        </Text>
         <HadithCard isHomeCard={false} />
 
         <Text style={styles.questionText}>
-            Understand: How would you describe this hadith to someone else?
+          Understand: How would you describe this hadith to someone else?
         </Text>
         <TextInput
-            value={editedEntry.question1}
-            onChangeText={(text) =>
+          value={editedEntry.question1}
+          onChangeText={(text) =>
             setEditedEntry({
-                ...editedEntry,
-                question1: text,
+              ...editedEntry,
+              question1: text,
             })
-            }
-            style={styles.viewEntryText}
-            multiline={true}
-        // Below is the way to add the correct background color styling,
-        // But for some reason, I can't see the text of the previous response
-        // style={{...styles.editEntryText, backgroundColor: '#F4F1DE', color: 'black'}}
+          }
+          style={styles.viewEntryText}
+          multiline={true}
+          // Below is the way to add the correct background color styling,
+          // But for some reason, I can't see the text of the previous response
+          // style={{...styles.editEntryText, backgroundColor: '#F4F1DE', color: 'black'}}
         />
 
         <Text style={styles.questionText}>
-            Reflect: How does this hadith relate to your past and present?
+          Reflect: How does this hadith relate to your past and present?
         </Text>
         <TextInput
-            value={editedEntry.question2}
-            onChangeText={(text) =>
+          value={editedEntry.question2}
+          onChangeText={(text) =>
             setEditedEntry({
-                ...editedEntry,
-                question2: text,
+              ...editedEntry,
+              question2: text,
             })
-            }
-            style={styles.viewEntryText}
-            multiline={true}
+          }
+          style={styles.viewEntryText}
+          multiline={true}
         />
 
         <Text style={styles.questionText}>
-            Action: How can you implement this hadith in your future?
+          Action: How can you implement this hadith in your future?
         </Text>
         <TextInput
-            value={editedEntry.question3}
-            onChangeText={(text) =>
+          value={editedEntry.question3}
+          onChangeText={(text) =>
             setEditedEntry({
-                ...editedEntry,
-                question3: text,
+              ...editedEntry,
+              question3: text,
             })
-            }
-            style={styles.viewEntryText}
-            multiline={true}
+          }
+          style={styles.viewEntryText}
+          multiline={true}
         />
-        
+
         <Button onPress={handleSave} style={styles.button}>
-            <Text color="#3D405B">Save</Text>
+          <Text color="#3D405B">Save</Text>
         </Button>
         <Button onPress={handleCancel} style={styles.button}>
-            <Text color="#3D405B">Cancel</Text>
+          <Text color="#3D405B">Cancel</Text>
         </Button>
-    </>
+      </>
     );
   }
 
@@ -115,51 +123,62 @@ function ViewEntry({ route, navigation }) {
     <ScreenWrapper>
       <ScrollView>
         <Stack direction="column" space={3}>
-          {isEditing ? <EditEntry />
-          : (
-              <>
-                <View flexDirection="row" alignItems="center" justifyContent="space-between" marginBottom="5"
-                  height="60">
-                  <Button
-                    startIcon={<ChevronLeftIcon />}
-                    _icon={{ color: "black" }}
-                    style={styles.iconButton}
-                    onPress={() => navigation.navigate('Your Journal')}
-                  />
-                  <Text fontSize="16">{route.params.date}</Text>
-                  {/* Hacky way of putting a third part */}
-                  <View width="10" />
-                </View>
-                <HadithCard isHomeCard={false} />
-                <Text style={styles.questionText}>Understand: How would you describe this hadith to someone else?</Text>
-                <Box>
-                  <Text style={styles.viewEntryText}>{editedEntry.question1}</Text>
-                </Box>
+          {isEditing ? (
+            <EditEntry />
+          ) : (
+            <>
+              <View
+                flexDirection="row"
+                alignItems="center"
+                justifyContent="space-between"
+                marginBottom="5"
+                height="60"
+              >
+                <Button
+                  startIcon={<ChevronLeftIcon />}
+                  _icon={{ color: 'black' }}
+                  style={styles.iconButton}
+                  onPress={() => navigation.navigate('Your Journal')}
+                />
+                <Text fontSize="16">{route.params.date}</Text>
+                {/* Hacky way of putting a third part */}
+                <View width="10" />
+              </View>
+              <HadithCard isHomeCard={false} />
+              <Text style={styles.questionText}>
+                Understand: How would you describe this hadith to someone else?
+              </Text>
+              <Box>
+                <Text style={styles.viewEntryText}>{editedEntry.question1}</Text>
+              </Box>
 
-                <Text style={styles.questionText}>Reflect: How does this apply to your past and present?</Text>
-                <Box>
-                  <Text style={styles.viewEntryText}>{editedEntry.question2}</Text>
-                </Box>
+              <Text style={styles.questionText}>
+                Reflect: How does this apply to your past and present?
+              </Text>
+              <Box>
+                <Text style={styles.viewEntryText}>{editedEntry.question2}</Text>
+              </Box>
 
-                <Text style={styles.questionText}>Action: How can you implement this hadith in your future?</Text>
-                <Box marginBottom="5">
-                  <Text style={styles.viewEntryText}>{editedEntry.question3}</Text>
-                </Box>
-              </>
-            )}
+              <Text style={styles.questionText}>
+                Action: How can you implement this hadith in your future?
+              </Text>
+              <Box marginBottom="5">
+                <Text style={styles.viewEntryText}>{editedEntry.question3}</Text>
+              </Box>
+            </>
+          )}
         </Stack>
       </ScrollView>
 
       {/* floating Edit button */}
-      {!isEditing &&
+      {!isEditing && (
         <Button
           onPress={() => setIsEditing(true)}
           style={[styles.iconButton, styles.floatBottomRight]}
           startIcon={<MaterialCommunityIcons name="pencil" size={32} color="black" />}
-          _icon={{ color: "black" }}
-        >
-        </Button>
-      }
+          _icon={{ color: 'black' }}
+        ></Button>
+      )}
 
       {/* floating Save button */}
       {/*isEditing &&
