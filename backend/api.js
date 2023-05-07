dc9331a3ccbc4c09399d5dcf86c994936cf1b9e6
Rumaisa Nazeeruddin <rumaisarn2@gmail.com>
2023-05-07 11:23:01 -0700

    quotes

--- backend/api.js
+++ backend/api.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 //connecting to heroku database
 const pool = new pg.Pool({
-  connectionString: 'postgres://taonknyscfdcbt:8d2638d1e2373a9ee7e1cbdb2afa5bd3d27256e75c4647fef7c8e4a82f314b28@ec2-35-169-9-79.compute-1.amazonaws.com:5432/d7r31vn4qfsn53',
+  connectionString: "postgres://taonknyscfdcbt:8d2638d1e2373a9ee7e1cbdb2afa5bd3d27256e75c4647fef7c8e4a82f314b28@ec2-35-169-9-79.compute-1.amazonaws.com:5432/d7r31vn4qfsn53",
   ssl: {
     rejectUnauthorized: false
   }
