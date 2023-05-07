7a821707fd370abfcb3e1e58f35909db174f7afc
Rumaisa Nazeeruddin <rumaisarn2@gmail.com>
2023-05-07 11:17:43 -0700

    quotes

--- backend/api.js
+++ backend/api.js
@@ -8,10 +8,15 @@ app.use(cors());
 
 //connecting to heroku database
 const pool = new pg.Pool({
-  connectionString: postgres://taonknyscfdcbt:8d2638d1e2373a9ee7e1cbdb2afa5bd3d27256e75c4647fef7c8e4a82f314b28@ec2-35-169-9-79.compute-1.amazonaws.com:5432/d7r31vn4qfsn53,
+  connectionString: 'postgres://taonknyscfdcbt:8d2638d1e2373a9ee7e1cbdb2afa5bd3d27256e75c4647fef7c8e4a82f314b28@ec2-35-169-9-79.compute-1.amazonaws.com:5432/d7r31vn4qfsn53',
   ssl: {
     rejectUnauthorized: false
   }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
 
 app.get('/hadith/today', (req, res) => {
@@ -23,7 +28,4 @@ app.get('/hadith/today', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+
