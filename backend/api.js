08e324456ea44190560e76bdf4abef681fdcb34a
Rumaisa Nazeeruddin <rumaisarn2@gmail.com>
2023-05-08 19:28:29 -0700

    hardcoding port

--- backend/api.js
+++ backend/api.js
@@ -9,7 +9,7 @@ app.use(cors());
 //connecting to heroku database
 const pool = new pg.Pool({
   connectionString: "postgres://taonknyscfdcbt:8d2638d1e2373a9ee7e1cbdb2afa5bd3d27256e75c4647fef7c8e4a82f314b28@ec2-35-169-9-79.compute-1.amazonaws.com:5432/d7r31vn4qfsn53",
-  port: process.env.PORT
+  port: 19000
   ssl: {
     rejectUnauthorized: false
   }
@@ -27,8 +27,8 @@ app.get('/hadith/today', (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 19000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 19000}`);
+app.listen(19000, () => {
+  console.log(`Server is running on port ${19000}`);
 });
 
 module.exports = app;
